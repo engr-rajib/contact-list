d70e5aac60e26c6beb7af0cae9a3c08e9fa0a5a9
engr-rajib <engr.rajibb@gmail.com>
2025-02-05 15:49:22 +0600

    1st Commit

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -9,7 +9,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Contact> contacts = List.empty();
+
+  List<Contact> contacts = List.empty(growable: true);
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Padding(
             padding: EdgeInsets.all(10),
             child: TextField(
+              controller: nameController,
               decoration: InputDecoration(
                 label: Text("Name"),
                 border: OutlineInputBorder(),
@@ -34,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Padding(
             padding: EdgeInsets.all(10),
             child: TextField(
+              controller: numberController,
               maxLength: 11,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
@@ -44,11 +50,64 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Padding(
             padding: EdgeInsets.all(10),
-            child: ElevatedButton(onPressed: () {},
-                child: Text("ADD"),),
+            child: ElevatedButton(
+              onPressed: () {
+              String name = nameController.text.trim();
+              String number = numberController.text.trim();
+              if (name.isNotEmpty && number.isNotEmpty && number.length == 11) {
+                setState(() {
+                  nameController.text = '';
+                  numberController.text = '';
+                  contacts.add(Contact(name: name, number: number));
+                });
+              }
+            },
+              child: Text("ADD",),
+            ),
+          ),
+          contacts.isEmpty ? Text('No Contacts') :
+          Expanded (
+            child: ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context, index) => getRow(index),
+            ),
           )
         ],
       ),
     );
+  }
+
+  Widget getRow (int index) {
+    return Card (
+      child: ListTile(
+        leading: Icon(Icons.person, size: 25,),
+        trailing: Icon(Icons.call, size: 25, color: Colors.green,),
+        title: Text (contacts[index].name, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red, fontSize: 20),),
+        subtitle: Text (contacts[index].number),
+        onLongPress: () {
+          showDialog(context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: Text("Confirmation", style: TextStyle(fontWeight: FontWeight.bold),),
+                content: Text("Do you want to Delete the selected Contact?", style: TextStyle(fontSize: 20),),
+                actions: [
+                  IconButton(
+                      onPressed: () {Navigator.of(context).pop();}, 
+                      icon: Icon(Icons.not_interested_outlined, color: Colors.green,)),
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        setState(() {
+                          contacts.removeAt(index);
+                        }
+                        );
+                      },
+                      icon: Icon(Icons.delete, color: Colors.red,))
+                ],
+              )
+          );
+        },
+        ),
+      );
+
   }
 }
